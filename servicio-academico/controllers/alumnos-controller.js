14a0847215b793a1ad3b79bf603f6293a39b0aeb
Dylan Balvidares <Dylanaza037@gmail.com>
2026-03-26 12:39:18 -0300

    serv-academico v2

--- servicio-academico/controllers/alumnos-controller.js
+++ servicio-academico/controllers/alumnos-controller.js
@@ -1,5 +1,149 @@
 import Alumno from "../models/alumnos-model";
 
-export async function obtenerAlumno(id) {
-  Alumno.findByPk(id);
+async function obtenerTodosAlumnos() {
+  try {
+    const alumno = await Alumno.findAll();
+
+    if (query.length === 0) {
+      throw new Error("No se encontraron alumnos");
+    }
+
+    return alumno;
+  } catch (error) {
+    console.error("Error en obtenerTodosAlumnos", error);
+    return "Error en obtenerTodosAlumnos";
+  }
 }
+
+async function obtenerAlumno(id) {
+  try {
+    const alumno = await Alumno.findByPk(id);
+
+    if (!alumno) {
+      throw new Error("Error en obtenerAlumno(id)");
+    }
+
+    return alumno;
+  } catch (error) {
+    console.error("Error en obtenerAlumno(id):", error);
+    return ("Error en obtenerAlumno(id):", error);
+  }
+}
+
+async function crearAlumno(alumno) {
+  try {
+    const data = await Alumno.create(alumno);
+
+    if (!data) {
+      return new Error("Error en crearAlumno");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en crearAlumno(alumno):", error);
+    return ("Error en crearAlumno(alumno):", error);
+  }
+}
+
+async function eliminarAlumno(id) {
+  try {
+    const data = await Alumno.destroy({
+      where: {
+        id_alumno: id,
+      },
+    });
+
+    if (data === 0) {
+      throw new Error("Error en eliminarAlumno(id)");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en eliminarAlumno(id)");
+    return "Error en eliminarAlumno(id)";
+  }
+}
+
+async function modificarNombre(id, nombre) {
+  try {
+    const data = await Alumno.update(
+      {
+        nombre_alumno: nombre,
+      },
+      {
+        where: {
+          id_alumno: id,
+        },
+      },
+    );
+
+    if (!data) {
+      throw new Error("Error en modificarNombre(id,nombre)");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en modificarNombre(id,nombre)");
+    return "Error en modificarNombre(id,nombre)";
+  }
+}
+
+async function modificarApellido(id, apellido) {
+  try {
+    const data = await Alumno.update(
+      {
+        apellido_alumno: apellido,
+      },
+      {
+        where: {
+          id_alumno: id,
+        },
+      },
+    );
+
+    if (!data) {
+      throw new Error("Error en modificarApellido(id,apellido)");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en modificarApellido(id,apellido)");
+    return "Error en modificarApellido(id,apellido)";
+  }
+}
+
+async function modificarDni(id, dni) {
+  try {
+    const data = await Alumno.update(
+      {
+        dni_alumno: apellido,
+      },
+      {
+        where: {
+          id_alumno: id,
+        },
+      },
+    );
+
+    if (!data) {
+      throw new Error("Error en modificarDni(id,dni)");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en modificarDni(id,dni)");
+    return "Error en modificarDni(id,dni)";
+  }
+}
+
+async function modificarAlumno(alumno) {}
+
+export {
+  obtenerTodosAlumnos,
+  obtenerAlumno,
+  crearAlumno,
+  modificarNombre,
+  modificarApellido,
+  modificarDni,
+  modificarAlumno,
+};
---
+++ servicio-academico/controllers/cursos-controller.js
@@ -0,0 +1,52 @@
+import Curso from "../models/cursos-model";
+
+async function crearCurso(curso) {
+  try {
+    const data = await Curso.create(curso); //<--create:build & save
+
+    return data;
+  } catch (error) {
+    console.error("Error en crearCurso(curso):", error);
+    return ("Error en crearCurso(curso):", error);
+  }
+}
+
+async function eliminarCurso(id) {
+  try {
+    const data = await Curso.destroy({
+      where: {
+        id_curso: id,
+      },
+    });
+
+    if (data === 0) {
+      throw new Error("Error en eliminarCurso(id)");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error en eliminarCurso(id)");
+    return "Error en eliminarCurso(id)";
+  }
+}
+
+async function modificarCurso(id, _curso) {
+  if ((!_curso) instanceof Curso) {
+    throw new Error("Error en modificarCurso(id,curso)");
+  }
+
+  const curso = Curso.build(_curso);
+
+  try {
+    const data = await Curso.update(
+      {
+        nombre_curso: curso.get.
+      },
+      {
+        where: {
+          id_curso: id,
+        },
+      },
+    );
+  } catch (error) {}
+}
